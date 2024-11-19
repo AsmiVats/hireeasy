1ea137a6384eb5542010c147f586c679beacde57
rishav-bhardwaz <rishav76000@gmail.com>
2024-11-19 09:37:23 +0530

    Bento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 import Header from '@/app/component/layout/Header'
 import HeroSection from '@/app/component/layout/Herosection';
+import Bento from './component/layout/Bento';
 
 export default function LandingPage() {
   return (
     <div className="bg-white">
       <Header />
       <HeroSection />
+      <Bento />
     </div>
   );
 }
